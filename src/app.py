8f707b9f1e4e3222e93569ea726987c548272304
sexareds <cesarrojasv60@gmail.com>
2023-01-16 03:24:29 -0400

    app

---
+++ src/app.py
@@ -0,0 +1,9 @@
+import modeling.q_1 as q_1
+import modeling.q_2 as q_2
+
+if __name__ == '__main__':
+    # q_1.get_students_EIU()
+    # q_1.get_students_gender()
+    # q_1.get_students_semester()
+    # q_1.get_students_career()
+    q_2.get_qualities()
