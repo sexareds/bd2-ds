d1a278bddca8faee4d701ed997c1b5dfa7cfde4b
sexareds <cesarrojasv60@gmail.com>
2023-01-16 03:23:56 -0400

    modeling files

---
+++ src/modeling/q_1.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import processing.q_1 as q_1
+
+def get_students_EIU():
+    df = q_1.get_students_EIU()
+    df.groupby(['Respuesta']).size().plot(kind='bar', subplots=True)
+    plt.title('Estudiantes que conocen el EIU')
+    plt.show()
+
+def get_students_gender():
+    df = q_1.get_students_gender()
+    df.groupby(['Genero']).size().plot(kind='pie', autopct='%1.1f%%')
+    plt.title('Genero de estudiantes que conocen el EIU')
+    plt.show()
+
+def get_students_semester():
+    df = q_1.get_students_semester()
+    df.groupby(['titulo']).size().plot(kind='bar', subplots=True)
+    plt.title('Semestre de estudiantes que conocen el EIU')
+    plt.show()
+
+def get_students_career():
+    df = q_1.get_students_career()
+    df.groupby(['titulo']).size().plot(kind='bar', subplots=True)
+    plt.title('Carrera de estudiantes que conocen el EIU')
+    plt.show()
---
+++ src/modeling/q_2.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import processing.q_2 as q_2
+
+def get_qualities():
+    df = q_2.get_qualities()
+    print(df['respuesta'])
+    # df['respuesta'].groupby(['respuesta']).size().plot(kind='pie', autopct='%1.1f%%')
+    # plt.title('Cualidades de estudiantes que conocen el EIU')
+    # plt.show()
