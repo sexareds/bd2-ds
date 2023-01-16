eda9e70e2bb85a1f67c5b0f110099aaef01591a7
sexareds <cesarrojasv60@gmail.com>
2023-01-16 03:24:15 -0400

    proccesing files

--- src/processing/q_1.py
+++ src/processing/q_1.py
@@ -1,18 +1,8 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import preparation.clstbl as ct
 
-# un metodo para saber de los que votaron 'si', cuantos votaron 'no' y cuantos no votaron
-
-# otro para saber de los que votaron 'si':
-# su genero
-# semestre
-# carrera
-
-def get_answers_EIU():
+def get_students_EIU():
     sql_script = """
-        SELECT Opcion.titulo AS Titulo
+        SELECT Opcion.titulo AS Respuesta
         FROM Respuesta
         INNER JOIN Opcion ON Respuesta.num_pregunta = Opcion.num_pregunta
         AND Respuesta.num_opcion = Opcion.num_opcion
@@ -23,5 +13,78 @@ def get_answers_EIU():
 
 def get_students_gender():
     sql_script = """
-        SELECT
+        SELECT T1.titulo AS Genero
+        FROM (
+            SELECT Respuesta.num_encuesta, Opcion.titulo
+            FROM Encuesta
+            INNER JOIN Respuesta
+            ON Encuesta.num_encuesta = Respuesta.num_encuesta
+            AND Respuesta.num_pregunta = 1
+            INNER JOIN Opcion
+            ON Respuesta.num_pregunta = Opcion.num_pregunta
+            AND Respuesta.num_opcion = Opcion.num_opcion
+        ) T1 
+        INNER JOIN (
+            SELECT Respuesta.num_encuesta, Opcion.titulo
+            FROM Respuesta
+            INNER JOIN Opcion ON Respuesta.num_pregunta = Opcion.num_pregunta
+            AND Respuesta.num_opcion = Opcion.num_opcion
+            AND Respuesta.num_pregunta = 4
+            AND Respuesta.num_opcion = 1
+        ) T2
+        ON T1.num_encuesta = T2.num_encuesta;
     """
+    df = ct.get_table(sql_script)
+    return df
+
+def get_students_semester():
+    sql_script = """
+        SELECT T1.num_encuesta, T1.titulo
+        FROM (
+            SELECT Respuesta.num_encuesta, Opcion.titulo
+            FROM Encuesta
+            INNER JOIN Respuesta
+            ON Encuesta.num_encuesta = Respuesta.num_encuesta
+            AND Respuesta.num_pregunta = 2
+            INNER JOIN Opcion
+            ON Respuesta.num_pregunta = Opcion.num_pregunta
+            AND Respuesta.num_opcion = Opcion.num_opcion
+        ) T1 
+        INNER JOIN (
+            SELECT Respuesta.num_encuesta, Opcion.titulo
+            FROM Respuesta
+            INNER JOIN Opcion ON Respuesta.num_pregunta = Opcion.num_pregunta
+            AND Respuesta.num_opcion = Opcion.num_opcion
+            AND Respuesta.num_pregunta = 4
+            AND Respuesta.num_opcion = 1
+        ) T2
+        ON T1.num_encuesta = T2.num_encuesta;
+    """
+    df = ct.get_table(sql_script)
+    return df
+
+def get_students_career():
+    sql_script = """
+        SELECT T1.num_encuesta, T1.titulo
+        FROM (
+            SELECT Respuesta.num_encuesta, Opcion.titulo
+            FROM Encuesta
+            INNER JOIN Respuesta
+            ON Encuesta.num_encuesta = Respuesta.num_encuesta
+            AND Respuesta.num_pregunta = 3
+            INNER JOIN Opcion
+            ON Respuesta.num_pregunta = Opcion.num_pregunta
+            AND Respuesta.num_opcion = Opcion.num_opcion
+        ) T1 
+        INNER JOIN (
+            SELECT Respuesta.num_encuesta, Opcion.titulo
+            FROM Respuesta
+            INNER JOIN Opcion ON Respuesta.num_pregunta = Opcion.num_pregunta
+            AND Respuesta.num_opcion = Opcion.num_opcion
+            AND Respuesta.num_pregunta = 4
+            AND Respuesta.num_opcion = 1
+        ) T2
+        ON T1.num_encuesta = T2.num_encuesta;
+    """
+    df = ct.get_table(sql_script)
+    return df
--- src/processing/q_2.py
+++ src/processing/q_2.py
@@ -1,7 +1,19 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from db import cx
+import preparation.clstbl as ct
 
-# un metodo para obtener las cualidades en un diagrama de pastel con sus porcentajes
+def get_qualities() -> None:
+    sql_script = """
+        SELECT *
+        FROM Respuesta
+        WHERE num_pregunta = 6;
+    """
+    df = ct.get_table(sql_script)
+    # df = ct.data_cleaning(df)
+    return df
+
+def get_students_semester() -> None:
+    sql_script = """
+
+    """
+    df = ct.get_table(sql_script)
+    df = ct.data_cleaning(df)
+    return df
