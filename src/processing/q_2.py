83749ccc813a76613f8def3e5d14894458c4c6d8
sexareds <cesarrojasv60@gmail.com>
2023-01-17 01:07:47 -0400

    Processing Files

--- src/processing/q_2.py
+++ src/processing/q_2.py
@@ -2,12 +2,12 @@ import preparation.clstbl as ct
 
 def get_qualities() -> None:
     sql_script = """
-        SELECT *
+        SELECT respuesta
         FROM Respuesta
         WHERE num_pregunta = 6;
     """
     df = ct.get_table(sql_script)
-    # df = ct.data_cleaning(df)
+    df = ct.data_cleaning(df)
     return df
 
 def get_students_semester() -> None:
