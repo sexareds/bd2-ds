ecdbf5852b149977857a0f798fa1cd828bd069cb
sexareds <cesarrojasv60@gmail.com>
2023-01-19 00:00:27 -0400

    App

--- src/app.py
+++ src/app.py
@@ -1,27 +1,10 @@
 import modeling.q_1 as q_1
 import modeling.q_2 as q_2
+import modeling.q_3 as q_3
 import pandas as pd
 from thefuzz import fuzz, process
 
 if __name__ == '__main__':
     # q_1.answer()
     # q_2.get_qualities()
-
-    data = {
-        'nombres': ['sesar rojas', 'sexar rojas', 'samuel', 'césar rojas', 'sesar roja', 'eduardo']
-    }
-
-    df = pd.DataFrame(data)
-    print(df, '\n')
-
-    names = []
-
-    for i in range(0, len(df)):
-        for j in range(i+1, len(df)):
-            ratio = fuzz.ratio(df['nombres'][i], df['nombres'][j])
-            if ratio > 80:
-                print(df['nombres'][i], df['nombres'][j], ratio)
-                names.append(df['nombres'][i])
-    
-    print(names)
-    
+    q_3.get_answer()
