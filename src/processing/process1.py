88a847171a8b14d7fe5e5af621ca883087711003
sexareds <cesarrojasv60@gmail.com>
2023-01-25 21:54:00 -0400

    Data processing files

--- src/processing/process1.py
+++ src/processing/process1.py
@@ -10,5 +10,11 @@ def get_students_gender():
 def get_students_semester():
     return ct.get_table(q1.get_students_semester)
 
+def get_students_semester_2():
+    return ct.get_table(q1.get_students_semester_2)
+
 def get_students_career():
     return ct.get_table(q1.get_students_career)
+
+def get_students_career_2():
+    return ct.get_table(q1.get_students_career_2)
