0b0eb8326be24293ba481d856ccf418093a3aa2e
sexareds <cesarrojasv60@gmail.com>
2023-01-25 21:52:42 -0400

    App

--- src/app.py
+++ src/app.py
@@ -1,10 +1,9 @@
 import modeling.model1 as m1
 import modeling.model2 as m2
 import modeling.model3 as m3
-import pandas as pd
 from thefuzz import fuzz, process
 
 if __name__ == '__main__':
-    # m1.answer()
+    m1.get_answer()
     m2.get_answer()
-    # m3.get_answer()
+    m3.get_answer()
